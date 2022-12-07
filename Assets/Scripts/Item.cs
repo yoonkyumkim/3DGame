ead795b9127268b2627652c3ddc6543fcdbd4213
Yoon Kyum <kukkkkkk@naver.com>
2022-12-07 20:05:40 +0900

    아이템 프리팹 제작

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type
+    {
+        Ammo,
+        Coin,
+        Grenagde,
+        Heart,
+        Weapon
+    }
+
+    public Type type;
+    public int value;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * 30 * Time.deltaTime);
+    }
+
+}
