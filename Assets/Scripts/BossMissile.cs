dcf1cfc84515dd710e424f3da121b596b6a2337a
Yoon Kyum <kukkkkkk@naver.com>
2022-12-27 19:24:48 +0900

    보스 탄환 및 공격 프리팹 추가

---
+++ Assets/Scripts/BossMissile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossMissile : Bullet
+{
+
+    public Transform target;
+    NavMeshAgent nav;
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(target.position);
+    }
+}
